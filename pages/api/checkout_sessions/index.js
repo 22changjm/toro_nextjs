d40bb9c57aedbf74ec5380472b7021cb9aabc623
Michael Chang <22mchang@gmail.com>
2022-01-10 22:54:28 -0800

    fix cancel route

--- pages/api/checkout_sessions/index.js
+++ pages/api/checkout_sessions/index.js
@@ -10,12 +10,11 @@ export default async function handler(req, res) {
                 payment_method_types: ['card'],
                 line_items: req?.body?.items ?? [],
                 success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-                cancel_url: `${req.headers.origin}/cart`,
+                cancel_url: `${req.headers.origin}/order`,
             });
 
             res.status(200).json(session);
         } catch (err) {
-            console.log(err.message)
             res.status(500).json({statusCode: 500, message: err.message});
         }
     } else {
