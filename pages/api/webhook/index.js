798f20a3fb3b7546bb37a2f8276d225939231d10
Michael Chang <22mchang@gmail.com>
2022-01-11 15:52:59 -0800

    done

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
         }
 
 
-        res.json({received: true});
+        res.json({set: data});
     } else {
         res.setHeader('Allow', 'POST');
         res.status(405).end('Method Not Allowed');
