a1dad3d8634b2dd025e17b4146029a87d47a2ec6
Michael Chang <22mchang@gmail.com>
2022-01-12 15:56:02 -0800

    added call api

---
+++ pages/api/call/index.js
@@ -0,0 +1,27 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            
+            client.messages
+                    .create({
+                        body: 'NEW ORDER! www.torofusiongrill.com/log',
+                        from: '+19378892658',
+                        to: '+19498707896'
+                    }).then(message => 
+                        res.status(200).json(message.sid));
+
+            
+        } catch (err) {
+            console.log(err.message)
+            res.status(500).json({statusCode: 500, message: err.message});
+        }
+    } else {
+        res.setHeader('Allow', 'GET');
+        res.status(405).end('Method Not Allowed');
+    }
+}
